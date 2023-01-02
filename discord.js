3ad4560f7a95a306d7d4c55077cc44d8e5bb30a8
vandal <vndljs@gmail.com>
2023-01-02 22:55:57 +0300

    wut
    upload

---
+++ discord.js
@@ -0,0 +1,17 @@
+/*
+~-~ This code was not written by me,
+~-~ The author of the code was lost.
+~-~ https://vnd1sm.space
+*/
+
+async function discord() {
+        try {
+            await navigator.clipboard.writeText('vandalism#6666');
+            copy.style.display='block';
+            setTimeout("copy.style.display='none'", 3333);
+        }
+        catch (err) {
+            console.error('errr...', err);
+            alert('vandalism#6666')
+        }
+}
